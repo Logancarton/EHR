9acf6dbfe9da7690a0836f2f587e5d77dc790e19
Logancarton <logan_1928@msn.com>
2026-09-06 13:20:02 -0700

    feat: activate workspace window manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,11 +3,13 @@ import DynamicSidebar from "./components/DynamicSidebar";
 import FloatingPaneController from "./components/FloatingPaneController";
 import ScrollExperienceManager from "./components/ScrollExperienceManager";
 import TabPointerController from "./components/TabPointerController";
+import WorkspaceWindowManager from "./components/WorkspaceWindowManager";
 import "./globals.css";
 import "./command-bar.css";
 import "./sidebar.css";
 import "./workspace-split.css";
 import "./scrollbars.css";
+import "./window-manager.css";
 
 export const metadata: Metadata = {
   title: "EHR Workspace",
@@ -20,6 +22,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
       <body>
         <DynamicSidebar />
         <FloatingPaneController />
+        <WorkspaceWindowManager />
         <ScrollExperienceManager />
         <TabPointerController />
         {children}
